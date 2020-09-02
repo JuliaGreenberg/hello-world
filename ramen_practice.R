23569a8977fe96014dacc66b8f265430ad3c2a1e
Greenberg <julia.greenberg@dc.gov>
2020-09-02 15:57:33 -0400

    Adding two comments

--- ramen_practice.R
+++ ramen_practice.R
@@ -6,3 +6,6 @@ country_name <- read_file("country_name.txt")
 ramen$Country <- as.character(ramen$Country)
 ramen$Country <- ifelse(ramen$Country==country_name,"Aus",
                         ifelse(ramen$Country=="Bangladesh","Ban",ramen$Country))
+
+# adding a comment now in master branch
+# two comments actually
