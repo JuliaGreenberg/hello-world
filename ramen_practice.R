3711e9a0100da3e1d72658540b9102b8cf7dadda
Greenberg <julia.greenberg@dc.gov>
2020-09-02 15:53:26 -0400

    Added comment to R file

--- ramen_practice.R
+++ ramen_practice.R
@@ -6,3 +6,5 @@ country_name <- read_file("country_name.txt")
 ramen$Country <- as.character(ramen$Country)
 ramen$Country <- ifelse(ramen$Country==country_name,"Aus",
                         ifelse(ramen$Country=="Bangladesh","Ban",ramen$Country))
+
+# just adding a comment
