23b546535b2b1430ac9945d1c2e05b4c7773946e
Greenberg <julia.greenberg@dc.gov>
2020-10-06 13:48:13 -0400

    Adding comment above others, only to master

--- ramen_practice.R
+++ ramen_practice.R
@@ -7,6 +7,8 @@ ramen$Country <- as.character(ramen$Country)
 ramen$Country <- ifelse(ramen$Country==country_name,"Aus",
                         ifelse(ramen$Country=="Bangladesh","Ban",ramen$Country))
 
+# ADDING COMMENT HERE, ONLY TO MASTER
+
 # adding a comment now in master branch
 # two comments actually
 
